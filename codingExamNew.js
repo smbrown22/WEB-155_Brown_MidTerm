855f8f543e1988417f7359fe0608f700986f6f19
smbrown22 <smbrown22@my.waketech.edu>
2025-04-02 13:57:29 -0400

    Add files via upload

--- codingExamNew.js
+++ codingExamNew.js
@@ -30,6 +30,10 @@ function prompting() {
         let last = loan - down 
         console.log(last)
 
+        var interest = 0.575
+
+        let monthlyPayment = (((interest / 12) * last) / (1 - Math.pow(1 + (interest / 12), (loanTerm * -12)))).toFixed(2)
+        console.log(monthlyPayment)
 
     }
     catch {
