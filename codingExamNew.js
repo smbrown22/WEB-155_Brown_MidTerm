71424e369605af305a2fbeafcdf2f2279a336597
smbrown22 <smbrown22@my.waketech.edu>
2025-04-03 12:08:21 -0400

    Add files via upload

--- codingExamNew.js
+++ codingExamNew.js
@@ -6,6 +6,7 @@ function prompting() {
 
         var loan = window.prompt("Enter loan value")
         var loan = Number(loan)
+        var principalLoanAmount = loan 
         console.log(loan)
     
         var downPayment = window.prompt("Enter Down Payment Value (Percentage)")
@@ -38,12 +39,11 @@ function prompting() {
         let monthly = loanTerm * 12 
 
         var interest = 0.575
-        let i = 0  
-        var monthlyPayment = (((interest / 12) * last) / (1 - Math.pow(1 + (interest / 12), (loanTerm * -12)))).toFixed(2) 
-        
-        for (let i = 0; i <= monthly; i++) {
-            var last = last - monthlyPayment
-            var monthlyPayment = (((interest / 12) * last) / (1 - Math.pow(1 + (interest / 12), (loanTerm * -12)))).toFixed(2)
+        var monthlyInterestRate = interest / 12
+
+        let monthlyPayment = ((monthlyInterestRate * last) / (1 - Math.pow(1 + monthlyInterestRate, -monthly))).toFixed(2)
+        for (let i = 0; i <= monthly && last > 0; i++) {
+            var last = last - (monthlyPayment)
             let result = document.createElement("p")
             let o = `Your monthly payment is ${monthlyPayment}$ and you are on Month ${i}, your remaining balance is ${last}$`
             result.textContent = o 
